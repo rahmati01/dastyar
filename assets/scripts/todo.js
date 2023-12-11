3ae31a6102a55e6a3514069467952ae910794841
unknown <rahmati.mr@gmail.com>
2023-12-11 23:05:30 +0330

    gesmat todo kamel anjam shode

---
+++ assets/scripts/todo.js
@@ -0,0 +1,9 @@
+const addBtn = document.querySelector(".plus-icon");
+console.log(addBtn);
+// const inputBox = document.querySelector("todo-input-box");
+// console.log(inputBox);
+
+// addBtn.addEventListener("click", () => {
+//   const userEnterTodo = inputBox.Value;
+//   //   console.log(userEnterTodo);
+// });
